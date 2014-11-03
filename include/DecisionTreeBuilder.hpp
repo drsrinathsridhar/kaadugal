f1743715b40361123eca2371cf86904f9dab2f1d
Srinath Sridhar <ssridhar@lap-14-42.mpi-inf.mpg.de>
2014-11-03 10:49:56 +0100

    Re-ordered methods. Time to finish BFS

--- include/DecisionTreeBuilder.hpp
+++ include/DecisionTreeBuilder.hpp
@@ -69,66 +69,6 @@ namespace Kaadugal
 
 	};
 
-	std::pair<std::shared_ptr<DataSetIndex>, std::shared_ptr<DataSetIndex>> Partition(std::shared_ptr<DataSetIndex> ParentDataSetIdx, const std::vector<VPFloat>& Responses, VPFloat Threshold)
-	{
-	    std::vector<int> LeftSubsetPts;
-	    std::vector<int> RightSubsetPts;
-	    for(int i = 0; i < ParentDataSetIdx->Size(); ++i)
-	    {
-		if(Responses[i] > Threshold) // Please use same logic when testing the tree
-		    LeftSubsetPts.push_back(ParentDataSetIdx->GetDataPointIndex(i));
-		else
-		    RightSubsetPts.push_back(ParentDataSetIdx->GetDataPointIndex(i));
-	    }
-
-	    return std::make_pair(std::shared_ptr<DataSetIndex>(new DataSetIndex(ParentDataSetIdx->GetDataSet(), LeftSubsetPts))
-				  , std::shared_ptr<DataSetIndex>(new DataSetIndex(ParentDataSetIdx->GetDataSet(), RightSubsetPts)));
-	};
-
-	const std::vector<VPFloat> SelectThresholds(std::shared_ptr<DataSetIndex> DataSubsetIdx, const std::vector<VPFloat>& Responses)
-	{
-	    // Please see Efficient Implementation of Decision Forests, Shotton et al. 2013
-	    // Section 21.3.3 explains how to implement this threshold selection using quantiles
-	    // Also see the Sherwood Library from Microsoft Research
-	    std::vector<VPFloat> Thresholds(m_Parameters.m_NumCandidateThresholds); // This is different from the Sherwood implementation. We don't use n+1
-	    std::vector<VPFloat> Quantiles(m_Parameters.m_NumCandidateThresholds + 1); // TODO: Candidate for memory saving
-
-	    // This isn't ideal because if size of data subset is only a few above NumThresh, then Randomizer will repeat some values
-	    if(DataSubsetIdx->Size() > m_Parameters.m_NumCandidateThresholds)
-	    {
-		// Sample m_NumCandidateThresholds+1 times (uniformly randomly) from Responses
-		std::uniform_int_distribution<int> UniDist(0, int(DataSubsetIdx->Size()-1)); // Both inclusive
-		for(int i = 0; i < m_Parameters.m_NumCandidateThresholds + 1; i++)
-		    Quantiles[i] = Responses[UniDist(Randomizer::Get().GetRNG())];
-	    }
-	    else
-	    {
-		Quantiles.resize(Responses.size());
-		Thresholds.resize(Responses.size()-1); // One less than quantiles
-		std::copy(Responses.begin(), Responses.end(), Quantiles.begin());
-	    }
-
-	    // Now compute quantiles. See https://www.stat.auckland.ac.nz/~ihaka/787/lectures-quantiles-handouts.pdf
-	    // if you don't know how to do this
-	    std::sort(Quantiles.begin(), Quantiles.end());
-
-	    if(Quantiles[0] == Quantiles[Quantiles.size()-1])
-		return std::vector<VPFloat>(); // Looks like samples were all the same. This is bad
-
-
-	    // Compute n candidate thresholds by sampling in between n+1 approximate quantiles
-	    std::uniform_real_distribution<VPFloat> UniRealDist(0, 1); // [0, 1), NOTE the exclusive end
-	    int NumThresholds = Thresholds.size();
-	    for(int i = 0; i < NumThresholds; ++i)
-		Thresholds[i] = Quantiles[i] + VPFloat(UniRealDist(Randomizer::Get().GetRNG()) * (Quantiles[i + 1] - Quantiles[i]));
-
-	    // std::cout << "Before return:\n";
-	    // for(int j = 0; j < Thresholds.size(); ++j)
-	    // 	std::cout << "Thresh: " << Thresholds[j] << std::endl;
-
-	    return Thresholds;
-	};
-
 	bool Build(std::shared_ptr<DataSetIndex> PartitionedDataSetIdx)
 	{
 	    m_Tree = std::shared_ptr<DecisionTree<T, S, R>>(new DecisionTree<T, S, R>(m_Parameters.m_MaxLevels));
@@ -351,26 +291,6 @@ namespace Kaadugal
 	    return true;
 	};
 
-	VPFloat GetObjectiveValue(S& ParentStats, S& LeftStats, S& RightStats)
-	{
-	    // Statistics are already aggregated
-	    // TODO: What if they are not aggregated?
-
-	    if(ParentStats.GetNumDataPoints() <= 0)
-		return 0.0;
-
-	    // NOTE: We are using information gain as the objective function
-	    // Change this and use a template/abstract class, if needed
-	    // See any of the Shotton et al. papers for this definition
-	    VPFloat InformationGain = ParentStats.GetEntropy()
-		- ( VPFloat(LeftStats.GetNumDataPoints())  * LeftStats.GetEntropy()
-		    + VPFloat(RightStats.GetNumDataPoints()) * RightStats.GetEntropy() ) / VPFloat(ParentStats.GetNumDataPoints());
-
-	    // std::cout << "InfoGain: " << InformationGain << std::endl;
-
-	    return InformationGain;
-	};
-
 	bool BuildTreeBreadthFirst(std::shared_ptr<DataSetIndex> DataSetIdx)
 	{
 	    // All the incoming data reaches the root for sure
@@ -387,7 +307,7 @@ namespace Kaadugal
 	{
 	    // TODO: Convert int to vector::type to avoid int overflow problems
 	    // TODO: Avoid using push_back()?
-	    // Using large integers
+	    // Using large integers - uint64_t
 	    int64_t NumCandidateThresholds = m_Parameters.m_NumCandidateThresholds>DataSetIdx->Size()?m_Parameters.m_NumCandidateThresholds:DataSetIdx->Size();
 	    int64_t NumSplitCandidates = m_Parameters.m_NumCandidateFeatures * NumCandidateThresholds;
 	    int64_t NumFrontierNodes = m_FrontierIdx.size();
@@ -418,7 +338,7 @@ namespace Kaadugal
 	    	    for(int k = 0; k < DataSetSize; ++k)
 	    		Responses.push_back(FeatureResponse.GetResponse(DataSetIdx->GetDataPoint(k))); // TODO: Can be parallelized/made more efficient?
 
-	    	    const std::vector<VPFloat>& Thresholds = SelectThresholds(PartitionedDataSetIdx, Responses);
+	    	    const std::vector<VPFloat>& Thresholds = SelectThresholds(DataSetIdx, Responses);
 	    	    int NumThresholds = Thresholds.size();
 	    	    for(int ThreshCtr = 0; ThreshCtr < NumThresholds; ++ThreshCtr)
 	    	    {
@@ -475,6 +395,86 @@ namespace Kaadugal
 		    m_FrontierIdx.push_back(2*NodeIndex + 2); // Right child
 		}
 	    }
+	};
+
+	std::pair<std::shared_ptr<DataSetIndex>, std::shared_ptr<DataSetIndex>> Partition(std::shared_ptr<DataSetIndex> ParentDataSetIdx, const std::vector<VPFloat>& Responses, VPFloat Threshold)
+	{
+	    std::vector<int> LeftSubsetPts;
+	    std::vector<int> RightSubsetPts;
+	    for(int i = 0; i < ParentDataSetIdx->Size(); ++i)
+	    {
+		if(Responses[i] > Threshold) // Please use same logic when testing the tree
+		    LeftSubsetPts.push_back(ParentDataSetIdx->GetDataPointIndex(i));
+		else
+		    RightSubsetPts.push_back(ParentDataSetIdx->GetDataPointIndex(i));
+	    }
+
+	    return std::make_pair(std::shared_ptr<DataSetIndex>(new DataSetIndex(ParentDataSetIdx->GetDataSet(), LeftSubsetPts))
+				  , std::shared_ptr<DataSetIndex>(new DataSetIndex(ParentDataSetIdx->GetDataSet(), RightSubsetPts)));
+	};
+
+	const std::vector<VPFloat> SelectThresholds(std::shared_ptr<DataSetIndex> DataSubsetIdx, const std::vector<VPFloat>& Responses)
+	{
+	    // Please see Efficient Implementation of Decision Forests, Shotton et al. 2013
+	    // Section 21.3.3 explains how to implement this threshold selection using quantiles
+	    // Also see the Sherwood Library from Microsoft Research
+	    std::vector<VPFloat> Thresholds(m_Parameters.m_NumCandidateThresholds); // This is different from the Sherwood implementation. We don't use n+1
+	    std::vector<VPFloat> Quantiles(m_Parameters.m_NumCandidateThresholds + 1); // TODO: Candidate for memory saving
+
+	    // This isn't ideal because if size of data subset is only a few above NumThresh, then Randomizer will repeat some values
+	    if(DataSubsetIdx->Size() > m_Parameters.m_NumCandidateThresholds)
+	    {
+		// Sample m_NumCandidateThresholds+1 times (uniformly randomly) from Responses
+		std::uniform_int_distribution<int> UniDist(0, int(DataSubsetIdx->Size()-1)); // Both inclusive
+		for(int i = 0; i < m_Parameters.m_NumCandidateThresholds + 1; i++)
+		    Quantiles[i] = Responses[UniDist(Randomizer::Get().GetRNG())];
+	    }
+	    else
+	    {
+		Quantiles.resize(Responses.size());
+		Thresholds.resize(Responses.size()-1); // One less than quantiles
+		std::copy(Responses.begin(), Responses.end(), Quantiles.begin());
+	    }
+
+	    // Now compute quantiles. See https://www.stat.auckland.ac.nz/~ihaka/787/lectures-quantiles-handouts.pdf
+	    // if you don't know how to do this
+	    std::sort(Quantiles.begin(), Quantiles.end());
+
+	    if(Quantiles[0] == Quantiles[Quantiles.size()-1])
+		return std::vector<VPFloat>(); // Looks like samples were all the same. This is bad
+
+
+	    // Compute n candidate thresholds by sampling in between n+1 approximate quantiles
+	    std::uniform_real_distribution<VPFloat> UniRealDist(0, 1); // [0, 1), NOTE the exclusive end
+	    int NumThresholds = Thresholds.size();
+	    for(int i = 0; i < NumThresholds; ++i)
+		Thresholds[i] = Quantiles[i] + VPFloat(UniRealDist(Randomizer::Get().GetRNG()) * (Quantiles[i + 1] - Quantiles[i]));
+
+	    // std::cout << "Before return:\n";
+	    // for(int j = 0; j < Thresholds.size(); ++j)
+	    // 	std::cout << "Thresh: " << Thresholds[j] << std::endl;
+
+	    return Thresholds;
+	};
+
+	VPFloat GetObjectiveValue(S& ParentStats, S& LeftStats, S& RightStats)
+	{
+	    // Statistics are already aggregated
+	    // TODO: What if they are not aggregated?
+
+	    if(ParentStats.GetNumDataPoints() <= 0)
+		return 0.0;
+
+	    // NOTE: We are using information gain as the objective function
+	    // Change this and use a template/abstract class, if needed
+	    // See any of the Shotton et al. papers for this definition
+	    VPFloat InformationGain = ParentStats.GetEntropy()
+		- ( VPFloat(LeftStats.GetNumDataPoints())  * LeftStats.GetEntropy()
+		    + VPFloat(RightStats.GetNumDataPoints()) * RightStats.GetEntropy() ) / VPFloat(ParentStats.GetNumDataPoints());
+
+	    // std::cout << "InfoGain: " << InformationGain << std::endl;
+
+	    return InformationGain;
 	};
 
 	bool BuildTreeHybrid(void)
