605224851e3244af40b06b0620134c6a801e81dd
Srinath Sridhar <ssridhar@wks-14-37.mpi-inf.mpg.de>
2015-03-26 19:33:24 +0100

    Fixing with double VPFloat

--- include/DecisionNode.hpp
+++ include/DecisionNode.hpp
@@ -8,7 +8,7 @@
 namespace Kaadugal
 {
     // Use Variable Precision Floating point representation
-    typedef float VPFloat; // Change to float or long double as needed
+    typedef double VPFloat; // Change to float or long double as needed
     enum NodeType
     {
 	SplitNode,
