3768e41e9d3cad3b2413f1c004d3fb8e04fa4f65
ssridhar <ssridhar@mpi-inf.mpg.de>
2016-11-21 17:35:53 +0100

    using double prec

--- include/DecisionNode.hpp
+++ include/DecisionNode.hpp
@@ -7,7 +7,8 @@
 namespace Kaadugal
 {
 	// Use Variable Precision Floating point representation
-	typedef float VPFloat; // Change to float or long double as needed
+	//typedef float VPFloat; // Change to float or long double as needed
+	typedef double VPFloat; // Change to float or long double as needed
 	enum NodeType
 	{
 		SplitNode,
