974843f38c1d761aa1b9f599c9f51f41bc6d9069
ssridhar <ssridhar@mpi-inf.mpg.de>
2016-11-21 18:24:59 +0100

    Added new defines

--- include/DecisionNode.hpp
+++ include/DecisionNode.hpp
@@ -3,12 +3,10 @@
 #include <limits>
 #include <cmath>
 #include <fstream>
+#include "Defines.hpp"
 
 namespace Kaadugal
 {
-	// Use Variable Precision Floating point representation
-	//typedef float VPFloat; // Change to float or long double as needed
-	typedef double VPFloat; // Change to float or long double as needed
 	enum NodeType
 	{
 		SplitNode,
